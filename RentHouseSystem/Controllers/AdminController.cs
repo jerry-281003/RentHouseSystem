92307446f7e7b0d50481f91993f1d2551d98e072
Jerry-281003 <thanhjerry03@gmail.com>
2024-12-12 12:01:44 +0700

    admin function Censorship of rental information

---
+++ RentHouseSystem/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentHouseSystem.Data;
+using RentHouseSystem.Models;
+
+namespace RentHouseSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RentHouseSystemContext _context;
+        public AdminController(RentHouseSystemContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> CheckRentalInfor()
+        {
+            var pendingHouses = _context.House
+        .Where(h => h.Status == status.pencding) 
+                                              
+        .ToList();
+
+            return View(pendingHouses);          
+                          
+        }
+        public async Task<IActionResult> Approved(string id)
+        {
+            var house = await _context.House
+                .FirstOrDefaultAsync(m => m.HouseId == id);
+            if (house == null)
+            {
+                house.Status = status.approved;
+                _context.Update(house);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Admin");
+        }
+        public async Task<IActionResult> Rejected(string id)
+        {
+            var house = await _context.House
+                .FirstOrDefaultAsync(m => m.HouseId == id);
+			if (house == null)
+			{
+				return NotFound(); 
+			}
+			house.Status = status.rejected;
+			_context.Update(house);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index", "Admin");
+        }
+    }
+}
